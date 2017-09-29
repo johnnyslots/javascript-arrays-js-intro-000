5acd088112edf92ad3d150b05e68a640cf85ab04
Yoni Slotwiner <yonislots@gmail.com>
2017-09-29 20:59:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,6 +19,6 @@ function destructivelyAddElementToEndOfArray(array, element){
 };
 
 function accessElementInArray(array, index){
-  array.indexOf(index);
-  return array
+  array[1];
+  return array;
 };
